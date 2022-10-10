2d6b7aef1b5c1ec13d5b19cac89f9d4bacdb9516
uturkarslan <ugurturkarslan@gmail.com>
2022-10-10 09:14:05 +0200

    Clean up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,13 +17,11 @@ newrelicPrometheusEndpointUs="https://metric-api.newrelic.com/prometheus/v1/writ
 newrelicPrometheusEndpointEu="https://metric-api.eu.newrelic.com/prometheus/v1/write?prometheus_server=${clusterName}"
 
 ### Prometheus ###
+
+# Update Helm dependencies
 # helm dependency update "../charts/prometheus"
 
-## Example 1
-# - Create ClusterRole and ClusterRoleBinding
-# - Install kube-state-metrics and node-exporter additionally
-# - Scrape everything
-# - Send data to 1 New Relic account without transformation
+# Install / upgrade Helm deployment
 helm upgrade prometheus \
   --install \
   --wait \
@@ -32,35 +30,7 @@ helm upgrade prometheus \
   --namespace $namespacePrometheus \
   --set kubeStateMetrics.enabled=true \
   --set nodeExporter.enabled=true \
+  --set newrelic.scrape_case="nodes_and_namespaces" \
   --set server.remoteWrite[0].url=$newrelicPrometheusEndpointEu \
   --set server.remoteWrite[0].bearer_token=$NEWRELIC_LICENSE_KEY \
   "../charts/prometheus"
-
-# helm template prometheus \
-#   --create-namespace \
-#   --namespace $namespacePrometheus \
-#   "../charts/prometheus" \
-#   | grep -A 100 '# Source: newrelic-prometheus-extended/templates/server/cm.yaml'
-
-## Example 2
-# - Create Role and RoleBinding
-# - Scrape only services, endpoints and pods
-# - Filter scraped data by 2 specific namespaces
-# - Send filtered data from 2 namespaces to 2 New Relic accounts
-# helm upgrade prometheus \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace $namespacePrometheus \
-#   --set server.remoteWrite[0].url="${newrelicPrometheusEndpointEu}namespace1" \
-#   --set server.remoteWrite[0].bearer_token=$NEWRELIC_LICENSE_KEY_1 \
-#   --set server.remoteWrite[0].write_relabel_configs[0].source_labels[0]="namespace" \
-#   --set server.remoteWrite[0].write_relabel_configs[0].regex=$namespaceBravo \
-#   --set server.remoteWrite[0].write_relabel_configs[0].action="keep" \
-#   --set server.remoteWrite[1].url="${newrelicPrometheusEndpointEu}namespace2" \
-#   --set server.remoteWrite[1].bearer_token=$NEWRELIC_LICENSE_KEY_2 \
-#   --set server.remoteWrite[1].write_relabel_configs[0].source_labels[0]="namespace" \
-#   --set server.remoteWrite[1].write_relabel_configs[0].regex=$namespaceCharlie \
-#   --set server.remoteWrite[1].write_relabel_configs[0].action="keep" \
-#   "../charts/prometheus"
